af2595f97d6966fd2200322493916d34572a71ff
Sunhwan Jo <sunhwanj@gmail.com>
2019-02-20 09:39:04 -0800

    initial checkin for planet dataset

---
+++ 03-planet/data/download.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+kaggle competitions download -c planet-understanding-the-amazon-from-space -f train-jpg.tar.7z -p .
+kaggle competitions download -c planet-understanding-the-amazon-from-space -f train_v2.csv -p .  
